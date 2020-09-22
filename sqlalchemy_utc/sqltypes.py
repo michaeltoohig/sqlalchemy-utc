a4f10e8b5e8e2922388c1c9c4a7ee82da6377f9a
michaeltoohig <34083701+michaeltoohig@users.noreply.github.com>
2020-09-23 09:35:55 +1100

    Update sqltypes.py
    Fixes issue number 6 in the original source project. The original has not accepted another user's pull request for over a year so I'll fork and use it here.

--- sqlalchemy_utc/sqltypes.py
+++ sqlalchemy_utc/sqltypes.py
@@ -33,4 +33,9 @@ class UtcDateTime(TypeDecorator):
     def process_result_value(self, value, dialect):
         if value is not None and value.tzinfo is None:
             value = value.replace(tzinfo=utc)
+        if value is not None:
+            if value.tzinfo is None:
+                value = value.replace(tzinfo=utc)
+            else:
+                value = value.astimezone(utc)
         return value
